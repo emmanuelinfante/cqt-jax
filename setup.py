93742338e7740e67a39c163c3f46cbfa63cc8861
Emmanuel Infante <163698214+emmanuelinfante@users.noreply.github.com>
2024-06-25 14:59:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="cqt-jax",
-    packages=find_packages(exclude=[]),
-    version="0.0.1",
-    license="MIT",
-    description="CQT JAX",
-    long_description_content_type="text/markdown",
+    version="0.1.0",
     author="emmanuelinfante",
     author_email="emanuel.06.educacion@gmail.com",
+    description="A JAX implementation of Constant-Q Transform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/emmanuelinfante/cqt-jax",
-    keywords=["artificial intelligence", "deep learning", "signal processing"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
     install_requires=[
         "jax>=0.2.21",
         "jaxlib>=0.1.71",
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
     ],
 )
